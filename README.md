944d98f44483040b9e063e1f8384d9210b2a823d
Gene Moynihan <g.moynihan2@gmail.com>
2020-04-18 10:24:24 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # MarketWatcher
- 
+
+Simple python script to grab closing price data from Yahoo finance and email it to a specific inbox.
+
+Default scheduling would be for this to run every morning pre market open.
+
+The default logic is based on the two variables defined at the top of the file in INTERVALS and TICKERS.
+
+TICKERS specifies the list of stocks you wish to track (any ticker from Yahoo finance should be fine to use here)
+
+INTERVALS specified the time frames you wish to compare prices over. For example with INTERVALs = [0, 1, 7, 30] the price movement for yesterdays close would be generated fothe prior day, week and month.
+
+Once all the data has been athered the two tables (one for last price and one for the historic price movements) are converted to html and send to the specified email.
+
+I would recommend setting up a throwaway gmail account to function as the email sender for security reasons as the password needs to be stored somewhere for this script to work.
---
+++ marketWatch.py
@@ -0,0 +1,105 @@
+###
+# LIBRARIES
+###
+
+import datetime as dt
+import yfinance as yf
+import pandas as pd
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import smptlib
+import ssl
+
+# Set the precision to 2 decimal places
+pd.options.display.float_format = '{:.2f}'.format
+
+###
+# GLOBAL VARS
+###
+
+# For email configuration and formatting
+TLS_PORT = 465 # no change
+EMAIL_PASS = "1235" # change
+SENDER_EMAIL = "name@mail.com" # change
+REC_EMAIL = "receiver@mail.com" # change
+MSG_TITLE = "Market Update - "+str(dt.datetime.today()) # no change
+
+# Time Intervals
+# Depending on the intervals you wish to compare against, change these
+# e.g. yesterday close compared to two days ago, one month ago etc...
+INTERVALS = [0, 1, 7, 30, 90, 180, 365]
+
+# Tickers to watch, lookup any new ones to add on Yahoo finance
+TICKERS = ['^FTSE', '^GSPC', '^IETP', 'HSBA.L']
+
+###
+# Functions
+###
+
+# Helper function to help adjust weekends to weekdays
+# Should be enhanced for trading holidays but I'm lazy...
+def getLastWorkingDate(date):
+    if date.weekday() == 5:
+        return date - dt.timedelta(days = 1)
+    elif date.weekday() == 6:
+        return date - dt.timedelta(days = 2)
+    else:
+        return date
+
+# Retrive the closing prices for the tickers for each of the
+#   days for the INTERVALS specified in the past
+def getCloses(last_working_date):
+    results = pd.DataFrame()
+    for i in INTERVALS:
+        date = last_working_date - dt.timedelta(days = i)
+        date = getLastWorkingDate(date)
+        print("Retrieving data for date:", date)
+        res = yf.download(TICKERS, start=date, end=date)
+        results = results.append(res["Adj Close"])
+    return results
+
+# Once closing prices are retrieved calculate the difference
+#   between last close and those prices.
+def getDiffs(closing_prices):
+    diff_results = pd.DataFrame()
+    diff_results["INTERVALS"] = INTERVALS
+    diff_results = diff_results.set_index('INTERVALS')
+    for nm in closing_prices:
+        print("Calculating differences for:", nm)
+        diffs=[]
+        current_price = closing_prices[nm][0]
+        for prc in closing_prices[nm]:
+            diff = 100*(current_price-prc)/prc
+            diffs.append(diff)
+        diff_results[nm] = diffs
+    return diff_results.drop(0)
+
+# Generate an email and send it via the gmail smtp server
+# I would recommend generating a dev gmail account for this purpose
+#   to avoid any chance of your main account being compromised
+def sendEmail(msg):
+    message = MIMEMultipart("alternative")
+    message["Subject"] = MSG_TITLE
+    message["From"] = SENDER_EMAIL
+    message["To"] = REC_EMAIL
+    msg = MIMEText(msg, "html")
+    message.attach(msg)
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", TLS_PORT, context=context) as server:
+        server.login(SENDER_EMAIL, EMAIL_PASS)
+        server.sendmail(SENDER_EMAIL, REC_EMAIL, message.as_string())
+
+# Grab last close price for all tickers specified as well as
+#   the differences (% change) for intervals in the last year
+# Once the data has been gathered change the tables to html and
+#   pass to the email send function
+def main():
+    last_working_date = dt.datetime.today() - dt.timedelta(days = 1)
+    last_working_date = getLastWeekday(last_working_date)
+    closing_prices = getCloses(last_working_date)
+    price_diffs = getDiffs(closing_prices)
+    closing_prices = closing_prices[0:1].to_html(formatters={'Name': lambda x: '<b>' + x + '</b>'})
+    price_diffs = price_diffs.to_html(formatters={'Name': lambda x: '<b>' + x + '</b>'})
+    sendEmail(closing_prices + price_diffs)
+
+main()
